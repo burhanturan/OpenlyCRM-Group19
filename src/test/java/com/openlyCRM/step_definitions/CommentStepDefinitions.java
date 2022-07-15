1e97aa51b334b3940887717c0dfff2cf7be9ecf6
Nihal Cantürk <97796688+nhlcntrk@users.noreply.github.com>
2022-07-15 17:38:25 +0300

    Comment page created

--- src/test/java/com/openlyCRM/step_definitions/CommentStepDefinitions.java
+++ src/test/java/com/openlyCRM/step_definitions/CommentStepDefinitions.java
@@ -5,7 +5,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class CommentStepDefinitions {
+public class CommentStepDefinitions{
 
 
     @When("user clicks comment")
